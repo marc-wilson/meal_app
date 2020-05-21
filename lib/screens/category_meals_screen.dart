3302434e1efe2c39199cd7b07db091b28bf3e0a0
Marc Wilson <marc@headset.io>
2020-05-21 15:00:11 -0700

    initial commit

---
+++ lib/screens/category_meals_screen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/dummy_data.dart';
+import 'package:meal_app/models/meal.dart';
+import 'package:meal_app/widgets/meal_item.dart';
+
+class CategoryMealsScreen extends StatefulWidget {
+  static const routeName = '/category-meals';
+  final List<Meal> availableMeals;
+
+  CategoryMealsScreen(this.availableMeals);
+
+  @override
+  _CategoryMealsScreenState createState() => _CategoryMealsScreenState();
+}
+
+class _CategoryMealsScreenState extends State<CategoryMealsScreen> {
+  String categoryTitle;
+  List<Meal> displayedMeals;
+  var _loadedInitData = false;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    if (!_loadedInitData) {
+      final routeArgs =
+      ModalRoute
+          .of(context)
+          .settings
+          .arguments as Map<String, String>;
+      categoryTitle = routeArgs['title'];
+      final categoryId = routeArgs['id'];
+      displayedMeals = widget.availableMeals.where((meal) {
+        return meal.categories.contains(categoryId);
+      }).toList();
+      _loadedInitData = true;
+    }
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(categoryTitle)),
+      body: ListView.builder(
+        itemBuilder: (ctx, index) {
+          final meal = displayedMeals[index];
+          return MealItem(
+            id: meal.id,
+            title: meal.title,
+            imageUrl: meal.imageUrl,
+            duration: meal.duration,
+            affordability: meal.affordability,
+            complexity: meal.complexity,
+          );
+        },
+        itemCount: displayedMeals.length,
+      ),
+    );
+  }
+}
---
+++ lib/screens/favorites_screen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:meal_app/models/meal.dart';
+import 'package:meal_app/widgets/meal_item.dart';
+
+class FavoritesScreen extends StatelessWidget {
+  final List<Meal> favoriteMeals;
+  FavoritesScreen(this.favoriteMeals);
+
+  @override
+  Widget build(BuildContext context) {
+    if (favoriteMeals.isEmpty) {
+      return Center(
+        child: Text('You have no favorites yet')
+      );
+    } else {
+      return ListView.builder(
+        itemBuilder: (ctx, index) {
+          final meal = favoriteMeals[index];
+          return MealItem(
+            id: meal.id,
+            title: meal.title,
+            imageUrl: meal.imageUrl,
+            duration: meal.duration,
+            affordability: meal.affordability,
+            complexity: meal.complexity,
+          );
+        },
+        itemCount: favoriteMeals.length,
+      );
+    }
+    return Center(child: Text('The Favorites'));
+  }
+}
